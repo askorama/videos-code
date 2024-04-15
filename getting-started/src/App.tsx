e96cb4b4feff057b3bd186160e50f523a37a545a
SaraVieira <hey@iamsaravieira.com>
2024-04-15 17:59:58 +0100

    add start

--- getting-started/src/App.tsx
+++ getting-started/src/App.tsx
@@ -1,32 +1,12 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Results } from "./components/Results";
 import { Input } from "./components/ui/input";
 import { Game } from "./lib/types";
 import { FAKE_GAMES } from "./lib/constants";
 
-import { OramaClient } from "@oramacloud/client";
-
-const client = new OramaClient({
-  endpoint: "https://cloud.orama.run/v1/indexes/ps1games-txegfr",
-  api_key: "sSmxk74DhVkBNYah9fQA604DDVhoMY7v",
-});
-
 export default function App() {
   const [query, setQuery] = useState("");
   const [games, setGames] = useState<{ document: Game }[]>([]);
-
-  const searchGames = async () => {
-    const results = await client.search({
-      term: query,
-      limit: 10,
-      tolerance: 1,
-    });
-    setGames(results?.hits || []);
-  };
-
-  useEffect(() => {
-    searchGames();
-  }, [query]);
 
   return (
     <div className="min-w-screen min-h-screen p-0 m-0 bg-background">
@@ -37,7 +17,7 @@ export default function App() {
           value={query}
           placeholder="Search for a PS1 Game"
         />
-        <Results games={games} />
+        <Results games={FAKE_GAMES} />
       </div>
     </div>
   );
