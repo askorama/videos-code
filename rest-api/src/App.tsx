8a87d5410ecb325d0614d6e27349d24ed7c84027
SaraVieira <hey@iamsaravieira.com>
2024-04-17 16:25:21 +0100

    add key

--- rest-api/src/App.tsx
+++ rest-api/src/App.tsx
@@ -6,7 +6,7 @@ import { FAKE_GAMES } from "./lib/constants";
 
 export default function App() {
   const [query, setQuery] = useState("");
-  const [games, setGames] = useState<{ document: Game }[]>([]);
+  const [games, setGames] = useState<{ document: Game; id: number }[]>([]);
 
   return (
     <div className="min-w-screen min-h-screen p-0 m-0 bg-background">
--- rest-api/src/components/Results.tsx
+++ rest-api/src/components/Results.tsx
@@ -1,7 +1,11 @@
 import { Game } from "@/lib/types";
 import { GameCard } from "./Game";
 
-export const Results = ({ games }: { games: { document: Game }[] }) => {
+export const Results = ({
+  games,
+}: {
+  games: { document: Game; id: number }[];
+}) => {
   return (
     <>
       <h1 className="text-foreground text-2xl font-bold pb-2">
@@ -9,7 +13,7 @@ export const Results = ({ games }: { games: { document: Game }[] }) => {
       </h1>
       <div className="grid grid-cols-4 gap-4">
         {games.map((game) => (
-          <GameCard game={game.document} />
+          <GameCard game={game.document} key={game.id} />
         ))}
       </div>
     </>
