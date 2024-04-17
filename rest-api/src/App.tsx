5d6064733a19fedc832085de5c392e2898455055
SaraVieira <hey@iamsaravieira.com>
2024-04-17 14:40:08 +0100

    update html

--- rest-api/src/App.tsx
+++ rest-api/src/App.tsx
@@ -39,7 +39,7 @@ export default function App() {
           type="search"
           onChange={(e) => setQuery(e.target.value)}
           value={query}
-          placeholder="Search for a PS1 Game"
+          placeholder="Search for a NES Game"
         />
         <Results games={games} />
       </div>
